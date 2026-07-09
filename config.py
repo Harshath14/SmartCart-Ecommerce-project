bf44d897e97435df87c47fba4ab01358ad9b173c
Harshath14 <cheedellaharshath4891@gmail.com>
2026-07-09 20:21:44 +0530

    smart

---
+++ config.py
@@ -0,0 +1,18 @@
+SECRET_KEY = "123456789"   # used for sessions
+
+# MySQL Database Configuration
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "123456789"  # keep empty if no password
+DB_NAME = "smartcart_db"
+
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'cheedellaharshath4891@gmail.com'
+MAIL_PASSWORD = 'jkvm leta mwum wpgn' 
+
+
+RAZORPAY_KEY_ID = "rzp_test_T8bk0xxQ3Qc4aj"
+RAZORPAY_KEY_SECRET = "C0L1xP5BmxYQ7FlGKPTLob0n"
